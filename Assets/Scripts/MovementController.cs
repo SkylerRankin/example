f94d4cac2c9ddd7e44b5f3322850bea0cf820339
Skyler <skylerrankin@gmail.com>
2017-10-21 12:48:35 -0400

    added unity project

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+    public float velocity;
+
+	// Use this for initialization
+	void Start () {
+        velocity = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * velocity * Time.deltaTime, 0f, 0f));
+        transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * velocity * Time.deltaTime, 0f));
+
+	}
+}
